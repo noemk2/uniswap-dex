8d5a35d9914aef487eb2928d674eef2852d3548d
Noe <noemk2@gmail.com>
2023-05-16 14:10:26 +0000

    ia omg

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import { computePoolAddress } from '@uniswap/v3-sdk'
-// import { ChainId, Fetcher, Token, WETH } from "@uniswap/sdk"
 import { SupportedChainId, Token } from '@uniswap/sdk-core'
-import { toReadableAmount, fromReadableAmount } from './conversion'
-// import Quoter from '@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json'
-import Quoter from '@uniswap/v3-periphery/artifacts/contracts/lens/QuoterV2.sol/QuoterV2.json'
 
-QUOTER_CONTRACT_ADDRESS = '0x82825d0554fA07f7FC52Ab63c961F330fdEFa8E8'
-POOL_FACTORY_CONTRACT_ADDRESS = '0xAfE208a311B21f13EF87E33A90049fC17A7acDEc'
 
 const setup = async () => {
 
@@ -20,78 +13,27 @@ const setup = async () => {
 
   const CELO = new Token(SupportedChainId.CELO_ALFAJORES, '0xF194afDf50B03e69Bd7D057c1Aa9e10c9954E4C9', 18, "CELO", "Celo Native asset")
 
-
-
-}
-
-const getPoolConstants = async () => {
-
-  const currentPoolAddress = computePoolAddress({
-    factoryAddress: POOL_FACTORY_CONTRACT_ADDRESS,
-    tokenA: CurrentConfig.tokens.in,
-    tokenB: CurrentConfig.tokens.out,
-    fee: CurrentConfig.tokens.poolFee,
-  })
-
-  const poolContract = new ethers.Contract(
-    currentPoolAddress,
-    IUniswapV3PoolABI.abi,
-    getProvider()
-  )
-  const [token0, token1, fee] = await Promise.all([
-    poolContract.token0(),
-    poolContract.token1(),
-    poolContract.fee(),
-  ])
-
-  return {
-    token0,
-    token1,
-    fee,
-  }
 }
 
 
-const quote = async () => {
-  const quoterContract = new ethers.Contract(
-    QUOTER_CONTRACT_ADDRESS,
-    Quoter.abi,
-    getProvider()
-  )
-  const poolConstants = await getPoolConstants()
+function App() {
+  const [loading, setLoading] = useState(true)
 
-  const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
-    poolConstants.token0,
-    poolConstants.token1,
-    poolConstants.fee,
-    fromReadableAmount(
-      CurrentConfig.tokens.amountIn,
-      CurrentConfig.tokens.in.decimals
-    ).toString(),
-    0
-  )
+  const [pair, setPair] = useState()
+  const [MTKu, setMTku] = useState()
 
+  useEffect(() => {
+    const variables = setup()
+    // setPair(variables.pair)
+    // setMTku(variables.MTKu)
 
+  }, [])
 
+  // console.log(pair)
+  // console.log(MTKu)
 
-  function App() {
-    const [loading, setLoading] = useState(true)
+  return <>{loading ? "Cargango... " : <p>Hola mundo </p>}</>;
 
-    const [pair, setPair] = useState()
-    const [MTKu, setMTku] = useState()
+}
 
-    useEffect(() => {
-      const variables = setup()
-      // setPair(variables.pair)
-      // setMTku(variables.MTKu)
-
-    }, [])
-
-    // console.log(pair)
-    // console.log(MTKu)
-
-    return <>{loading ? "Cargango... " : <p>Hola mundo </p>}</>;
-
-  }
-
-  export default App;
+export default App;
---
+++ src/quote.js
@@ -0,0 +1,71 @@
+import { ethers} from 'ethers'
+import { computePoolAddress } from '@uniswap/v3-sdk'
+import { toReadableAmount, fromReadableAmount } from './conversion'
+// import Quoter from '@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json'
+import Quoter from '@uniswap/v3-periphery/artifacts/contracts/lens/QuoterV2.sol/QuoterV2.json'
+
+import IUniswapV3PoolABI from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json'
+
+
+const QUOTER_CONTRACT_ADDRESS = '0x82825d0554fA07f7FC52Ab63c961F330fdEFa8E8'
+const POOL_FACTORY_CONTRACT_ADDRESS = '0xAfE208a311B21f13EF87E33A90049fC17A7acDEc'
+
+export async function quote(
+    tokenA,
+    tokenB,
+    fee,
+    amountIn,
+    provider
+) {
+    const quoterContract = new ethers.Contract(
+        QUOTER_CONTRACT_ADDRESS,
+        Quoter.abi,
+        provider
+    )
+
+    const poolConstants = await getPoolConstants(tokenA, tokenB, fee, provider)
+
+    const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
+        poolConstants.token0,
+        poolConstants.token1,
+        poolConstants.fee,
+        fromReadableAmount(
+            amountIn,
+            tokenA.decimals
+        ).toString(),
+        0
+    )
+
+    return toReadableAmount(quotedAmountOut, tokenB.decimals)
+}
+
+async function getPoolConstants(
+    tokenA,
+    tokenB,
+    fee,
+    provider
+) {
+    const currentPoolAddress = computePoolAddress({
+        factoryAddress: POOL_FACTORY_CONTRACT_ADDRESS,
+        tokenA,
+        tokenB,
+        fee,
+    })
+
+    const poolContract = new ethers.Contract(
+        currentPoolAddress,
+        IUniswapV3PoolABI.abi,
+        provider
+    )
+    const [token0, token1, poolFee] = await Promise.all([
+        poolContract.token0(),
+        poolContract.token1(),
+        poolContract.fee(),
+    ])
+
+    return {
+        token0,
+        token1,
+        fee: poolFee,
+    }
+}
